b2f5b0333ba1d9dd091997e8a4a72bcfcaec3234
Yamid Pico <ypicoleal@gmail.com>
2017-04-14 13:31:30 -0500

    Login animation 2 the arists

--- app/src/main/java/com/github/ypicoleal/logindemo/MainActivity.java
+++ app/src/main/java/com/github/ypicoleal/logindemo/MainActivity.java
@@ -281,10 +281,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        a.setDuration(400);
-        a2.setDuration(172);
-        a3.setDuration(400);
-        a4.setDuration(400);
+        a.setDuration(4000);
+        a2.setDuration(1720);
+        a3.setDuration(4000);
+        a4.setDuration(4000);
 
         a4.start();
         a3.start();
@@ -293,6 +293,222 @@ public class MainActivity extends AppCompatActivity {
         animationFirstArist.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate_first_arist));
         animationSecondArist.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate_second_arist));
         singupFormContainer.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate_form));
+    }
+
+    public void showLogIn(View view) {
+        final View animationCircle = findViewById(R.id.animation_circle);
+        final View animationFirstArist = findViewById(R.id.animation_first_arist);
+        final View animationSecondArist = findViewById(R.id.animation_second_arist);
+        final View animationSquare = findViewById(R.id.animation_square);
+        final TextView animationTV = (TextView) findViewById(R.id.animation_tv);
+        final ImageView twitterImageView = (ImageView) findViewById(R.id.twitter_img);
+        final ImageView instagramImageView = (ImageView) findViewById(R.id.instagram_img);
+        final ImageView facebokImageView = (ImageView) findViewById(R.id.facebook_img);
+        final View singupFormContainer = findViewById(R.id.signup_form_container);
+        final View loginFormContainer = findViewById(R.id.login_form_container);
+
+
+        animationFirstArist.setVisibility(View.VISIBLE);
+        findViewById(R.id.login_container).setVisibility(View.INVISIBLE);
+        animationCircle.setVisibility(View.VISIBLE);
+        animationSecondArist.setVisibility(View.VISIBLE);
+        /*animationSquare.setVisibility(View.VISIBLE);
+        animationTV.setVisibility(View.VISIBLE);
+        singupFormContainer.setVisibility(View.VISIBLE);*/
+
+        animationFirstArist.bringToFront();
+        animationCircle.bringToFront();
+        animationSecondArist.bringToFront();
+
+        final float scale = getResources().getDisplayMetrics().density;
+
+        final int circle_curr_margin = rootLayout.getWidth() - (int) (view.getWidth() - view.getX() - animationCircle.getWidth());
+        final int circle_target_margin = 0;
+
+        final int first_curr_width = (int) (108 * scale + 0.5f);
+        final int first_target_width = (int) (rootLayout.getHeight() * 2);
+
+        final int first_curr_height = (int) (70 * scale + 0.5f);
+        final int first_target_height = rootLayout.getWidth();
+
+        final int first_curr_margin = (int) (40 * scale + 0.5f);
+        final int first_target_margin = (int) (35 * scale + 0.5f);
+        final int first_expand_margin = (first_curr_margin - first_target_height);
+        final int first_curr_margin_r = rootLayout.getWidth() - first_curr_width;
+
+
+        final int square_target_width = rootLayout.getWidth();
+        final int square_target_height = (int) (80 * scale + 0.5f);
+
+        final float tv_curr_x = findViewById(R.id.singup_tv).getX() + findViewById(R.id.singup_button).getX();
+        final float tv_curr_y = findViewById(R.id.singup_tv).getY() + findViewById(R.id.buttons_container).getY() + findViewById(R.id.singup_container).getY();
+
+        final float tv_target_x = findViewById(R.id.singup_big_tv).getX();
+        final float tv_target_y = findViewById(R.id.singup_big_tv).getY();
+
+        final float tv_curr_size = 16;
+        final float tv_target_size = 56;
+
+        final int tv_curr_color = Color.parseColor("#ffffff");
+        final int tv_target_color = Color.parseColor("#5cffffff");
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorAccentDark));
+        }
+
+        Animation a = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                int diff_margin = circle_curr_margin - circle_target_margin;
+                int margin = circle_target_margin + (int) (diff_margin - (diff_margin * interpolatedTime));
+
+                RelativeLayout.LayoutParams params_circle = (RelativeLayout.LayoutParams) animationCircle.getLayoutParams();
+                params_circle.setMargins(0, 0, margin, (int) (40 * scale + 0.5f));
+                animationCircle.requestLayout();
+
+
+                int diff_width = first_curr_width - first_target_width;
+                int width = first_target_width + (int) (diff_width - (diff_width * interpolatedTime));
+
+                int diff_height = first_curr_height - first_target_height;
+                int height = first_target_height + (int) (diff_height - ((diff_height - first_target_margin) * interpolatedTime));
+
+                diff_margin = first_curr_margin - first_expand_margin;
+                margin = first_expand_margin + (int) (diff_margin - (diff_margin * interpolatedTime));
+                int margin_r = first_curr_margin_r - (int) (first_curr_margin_r * interpolatedTime);
+                int margin_l = rootLayout.getWidth() - width < 0 ? rootLayout.getWidth() - width : 0;
+
+                RelativeLayout.LayoutParams params_first = (RelativeLayout.LayoutParams) animationFirstArist.getLayoutParams();
+                params_first.setMargins(margin_l, 0, margin_r, margin);
+                params_first.width = width;
+                params_first.height = height;
+                animationFirstArist.requestLayout();
+
+                animationFirstArist.setPivotX(animationFirstArist.getWidth());
+                animationFirstArist.setPivotY(0);
+                animationFirstArist.setRotation(90 * interpolatedTime);
+
+                animationSecondArist.setPivotX(animationFirstArist.getWidth());
+                animationFirstArist.setPivotY(0);
+                animationFirstArist.setRotation(90 * interpolatedTime);
+
+                margin = first_curr_margin + (int) (first_target_margin * interpolatedTime);
+                RelativeLayout.LayoutParams params_second = (RelativeLayout.LayoutParams) animationSecondArist.getLayoutParams();
+                params_second.setMargins(0, 0, margin_r, margin);
+                params_second.width = width;
+                animationSecondArist.requestLayout();
+
+                animationSecondArist.setPivotX(animationSecondArist.getWidth());
+                animationSecondArist.setPivotY(animationSecondArist.getHeight());
+                animationSecondArist.setRotation(0 - (90 * interpolatedTime));
+
+                /*animationSquare.getLayoutParams().width = (int) (square_target_width * interpolatedTime);
+                animationSquare.requestLayout();
+
+                float diff_x = tv_curr_x - tv_target_x;
+                float x = tv_target_x + (diff_x - (diff_x * interpolatedTime));
+                float diff_y = tv_curr_y - tv_target_y;
+                float y = tv_target_y + (diff_y - (diff_y * interpolatedTime));
+
+                animationTV.setX(x);
+                animationTV.setY(y);
+                animationTV.requestLayout();
+
+                if (interpolatedTime >= 0.2f && interpolatedTime < 0.3f) {
+                    twitterImageView.setImageResource(R.drawable.ic_twitter_blue);
+                } else if (interpolatedTime >= 0.45f && interpolatedTime < 0.55f) {
+                    instagramImageView.setImageResource(R.drawable.ic_instagram_blue);
+                } else if (interpolatedTime >= 0.65f && interpolatedTime < 0.75f) {
+                    facebokImageView.setImageResource(R.drawable.ic_facebook_blue);
+                }
+
+                singupFormContainer.setAlpha(interpolatedTime);
+                loginFormContainer.setAlpha(1 - interpolatedTime);*/
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        /*a.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        animationCircle.setVisibility(View.GONE);
+                        animationFirstArist.setVisibility(View.GONE);
+                        animationSecondArist.setVisibility(View.GONE);
+                        animationTV.setVisibility(View.GONE);
+                        animationSquare.setVisibility(View.GONE);
+                        findViewById(R.id.singup_big_tv).setVisibility(View.VISIBLE);
+                    }
+                }, 100);
+                rootLayout.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
+                ((View) animationSquare.getParent()).setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorPrimary));
+                findViewById(R.id.login_form_container).setVisibility(View.GONE);
+                findViewById(R.id.login_tv).setVisibility(View.GONE);
+                showLoginButton();
+            }
+        });
+
+        Animation a2 = new Animation() {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                animationSquare.getLayoutParams().height = (int) (square_target_height * interpolatedTime);
+                animationSquare.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        ValueAnimator a3 = ValueAnimator.ofFloat(tv_curr_size, tv_target_size);
+        a3.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float animatedValue = (float) animation.getAnimatedValue();
+                animationTV.setTextSize(animatedValue);
+            }
+        });
+
+        ValueAnimator a4 = ValueAnimator.ofInt(tv_curr_color, tv_target_color);
+        a4.setEvaluator(new ArgbEvaluator());
+        a4.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int animatedValue = (int) animation.getAnimatedValue();
+                animationTV.setTextColor(animatedValue);
+            }
+        });*/
+
+        a.setDuration(4000);
+        /*a2.setDuration(1720);
+        a3.setDuration(4000);
+        a4.setDuration(4000);
+
+        a4.start();
+        a3.start();*/
+        //animationSquare.startAnimation(a2);
+        animationCircle.startAnimation(a);
+        //animationFirstArist.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate_first_reverse));
+        //animationSecondArist.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate_second_arist));
+        //singupFormContainer.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate_form));*/
     }
 
     private void showLoginButton() {
